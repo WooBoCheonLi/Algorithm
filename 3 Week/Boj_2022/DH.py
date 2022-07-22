c5df8ab7db47233adb7f69061cc54c80eada9bca
dahyekim1oo2 <97012327+dahyekim1oo2@users.noreply.github.com>
2022-07-22 20:34:26 +0900

    Update DH.py

--- 3 Week/Boj_2022/DH.py
+++ 3 Week/Boj_2022/DH.py
@@ -1,22 +1,22 @@
 from math import sqrt
-
 x, y, c = map(float, input().split())
 start, end = 0, min(x, y)
-res = 0
 
-def c(mid):
+
+def get_c(mid):
     a = sqrt(x**2-mid**2)
     b = sqrt(y**2-mid**2)
     return a*b/(a+b)
 
 
-
-while end-start > 1e-6:
+result = 0
+#오차가 10**-3인데 c()에서 제곱을 취해줌
+while end-start > 10**-6:
     mid = (start+end)/2
-    if c(mid) >= c:
-        res = mid
+    if get_c(mid) >= c:
+        result = mid
         start = mid
     else:
         end = mid
 
-print("{}".format(round(result, 4)))
+print("{}".format(round(result, 4))) #4번째 자리에서 반올림
